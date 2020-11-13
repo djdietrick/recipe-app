a57e23a58db2d5a8eed567642bce9191fbd044cd
Dave Dietrick <djdietrick@gmail.com>
2020-11-12 23:37:09 -0500

    Initialized project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase/app';
+import 'firebase/functions';
+import 'firebase/database';
+import 'firebase/auth';
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: "dash-website-1b9cb.firebaseapp.com",
+  databaseURL: "https://dash-website-1b9cb.firebaseio.com",
+  projectId: "dash-website-1b9cb",
+  storageBucket: "dash-website-1b9cb.appspot.com",
+  messagingSenderId: process.env.VUE_APP_MESSENGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+});
+
+export const functions = firebase.functions();
+export const db = firebase.database();
+export const auth = firebase.auth();
+
 Vue.config.productionTip = false
 
 new Vue({
